4b102c5b239f95047f2b2476027b33c050e9d0c1
Eduardo Gonzalez <egonzalez@sqli.com>
2024-10-28 17:07:32 +0100

    adding a new mame to made it different

---
+++ fundamentals/01-why_typescript_edu.ts
@@ -0,0 +1,8 @@
+
+const cursoName = "Typescript Bootcamp";
+
+printCursoName(cursoName);
+
+function printCursoName(nameNew : string) {
+    console.log("The course name is " + nameNew.toUpperCase());
+}
